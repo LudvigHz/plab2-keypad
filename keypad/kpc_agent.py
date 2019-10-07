421f1245f739206bf90dc17322b6a1faa04869cf
stianbm <stian.malmanger@gmail.com>
2019-10-07 19:37:33 +0200

    Pulled master and prettified for PyLint

--- keypad/kpc_agent.py
+++ keypad/kpc_agent.py
@@ -1,18 +1,24 @@
+"""Module contains the KPC agent class"""
+
+
 class KPCAgent:
     """The agent communicating with the finite state machine, keypad and led board"""
 
     def _init_passcode_entry(self):
-        """ Clear the passcode-buffer and initiate a ”power up” lighting sequence on the LED Board"""
+        """ Clear the passcode-buffer and initiate a ”power up” lighting sequence on the LED
+        Board"""
 
     def _get_next_signal(self):
-        """ Return the override-signal, if it is non-blank; otherwise query the keypad for the next pressed key"""
+        """ Return the override-signal, if it is non-blank; otherwise query the keypad for the next
+        pressed key"""
 
     def _verify_login(self):
-        """Check that the password just entered via the keypad matches that in the password file. Store the result
-        (Y or N) in the override-signal and light the leds appropriately"""
+        """Check that the password just entered via the keypad matches that in the password file.
+        Store the result (Y or N) in the override-signal and light the leds appropriately"""
 
     def _validate_passcode_change(self):
-        """Check that the new passcode is legal, if so update passcode_file, light leds appropriately"""
+        """Check that the new passcode is legal, if so update passcode_file, light leds
+        appropriately"""
 
     def _light_one_led(self):
         """Light led according to led_id and led_duration"""
@@ -25,4 +31,3 @@ class KPCAgent:
 
     def _exit_action(self):
         """Play the "power down" lighting sequence"""
-
